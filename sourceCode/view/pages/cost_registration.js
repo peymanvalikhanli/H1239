1a6d6e60e48e70d26cce78cb045743eaea8d2c7c
peymanvalikhanli <peymanvalikhanli2012@gmail.com>
2018-09-10 15:31:06 +0430

    check it ui design

--- sourceCode/view/pages/cost_registration.js
+++ sourceCode/view/pages/cost_registration.js
@@ -1,37 +1,37 @@
 import React, { PureComponent } from 'react';
 import { Image, View, Dimensions, StyleSheet, AsyncStorage, } from 'react-native';
-import { Container, Header, Content, Body, Label, Form, Button, Input, Item, Text, Right, Icon, Left, Footer, List, ListItem, Picker} from 'native-base';
+import { Container, Header, Content, Body, Label, Form, Button, Input, Item, Text, Right, Icon, Left, Footer, List, ListItem, Picker } from 'native-base';
 import Orientation from 'react-native-orientation';
 
 import JalaliCalendarPicker from 'react-native-jalali-calendar-picker';
 
 import lang from '../../model/lang/fa.json';
 
-import {Collapse,CollapseHeader, CollapseBody, AccordionList} from 'accordion-collapse-react-native';
+import { Collapse, CollapseHeader, CollapseBody, AccordionList } from 'accordion-collapse-react-native';
 
 import axios from 'axios';
 
-import server_url from '../../model/server_config/controller_url.json'; 
+import server_url from '../../model/server_config/controller_url.json';
 
 
 export default class cost_registration extends PureComponent {
 
-    get_data_from_server(){ 
+    get_data_from_server() {
         axios.post(server_url.GetTariffCategoryList, {
             userkey: this.state.Token,
         })
-        .then(response=> {
-            
-            if(response.data.act != undefined || response.data.act != null){
-                //alert(response.data.TariffCategory);
-                 if(response.data.TariffCategory != undefined || response.data.TariffCategory != null || response.data.TariffCategory != ''){
-                     this.setState({TariffCategory:response.data.TariffCategory}); 
-                 }
-            }         
-        })
-        .catch(function (error) {
-            console.log(error);
-        });
+            .then(response => {
+
+                if (response.data.act != undefined || response.data.act != null) {
+                    //alert(response.data.TariffCategory);
+                    if (response.data.TariffCategory != undefined || response.data.TariffCategory != null || response.data.TariffCategory != '') {
+                        this.setState({ TariffCategory: response.data.TariffCategory });
+                    }
+                }
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
     }
 
     constructor() {
@@ -40,19 +40,20 @@ export default class cost_registration extends PureComponent {
         this.onDateChange = this.onDateChange.bind(this);
 
         this.state = {
-            Token:'#',
+            Token: '#',
             national_code: '#',
             selectedStartDate: null,
-            price:'',
-            picker:'',
-            TariffCategory:[{Title:'',TariffCategoryTypeTitle:''}],
-          };
-        
+            price: '',
+            picker: '',
+            TariffCategory: [{ Title: '', TariffCategoryTypeTitle: '' }],
+            selectedPicker: 'test',
+        };
+
         AsyncStorage.getItem('Token', (err, result) => {
-            if(result!= null){
-               this.setState({Token: result});
-               this.get_data_from_server();
-           }  
+            if (result != null) {
+                this.setState({ Token: result });
+                this.get_data_from_server();
+            }
         });
 
 
@@ -60,28 +61,32 @@ export default class cost_registration extends PureComponent {
 
     onDateChange(date) {
         this.setState({
-          selectedStartDate: date,
+            selectedStartDate: date,
         });
-      }
+    }
 
-    btn_send_onclick(){
+    btn_send_onclick() {
         this.props.navigation.replace("home");
     }
 
-    create_currency_input(){ 
-     
-    } 
+    create_currency_input() {
+        var x = this.state.price;
+        var a = x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+        this.setState({ price: a });
+    }
 
 
-    get_picker(){
-        // style={stylesTitle.form_input}
-        const items = this.state.TariffCategory.map((s,i)=>{
-            return(
-                <Picker.Item label={lang.cost_type} key={i} label={s.Title} value={s.TariffCategoryTypeTitle} />
+    get_picker() {
+        const items = this.state.TariffCategory.map((s, i) => {
+            return (
+                <Picker.Item label={s.Title} value={s.Title} />
             );
         });
         return items;
-       
+    }
+
+    test() {
+        alert(this.state.selectedPicker)
     }
 
     render() {
@@ -93,93 +98,90 @@ export default class cost_registration extends PureComponent {
         var userProfile = this.props.navigation.state.params.userProfile;
 
         let date_picker = this.get_picker();
- 
+
         return (
             <Container style={{ flex: 1 }}>
                 <Header>
-                <Left>
-                    <Button
-                        onPress={()=>{this.props.navigation.replace("profile",{userId:userid ,userProfile: userProfile});}}
+                    <Left>
+                        <Button
+                            onPress={() => { this.props.navigation.replace("profile", { userId: userid, userProfile: userProfile }); }}
                         >
                             <Icon name="arrow-back" />
                         </Button>
-                        </Left> 
-                        <Body>
+                    </Left>
+                    <Body>
                         <Text
-                        style= {{textAlign:'center',color:'#ffffff',fontFamily: "DinarTwoMedium_MRT",}}
+                            style={{ textAlign: 'center', color: '#ffffff', fontFamily: "DinarTwoMedium_MRT", }}
                         >
                             {lang.cost_registration}
                         </Text>
                     </Body>
                     <Right>
                         <Button
-                        onPress={()=>{this.props.navigation.replace("home");}}
+                            onPress={() => { this.props.navigation.replace("home"); }}
                         >
                             <Icon name="home" />
                         </Button>
                     </Right>
-                    
-                </Header> 
-                <Content 
-                    style={{paddingLeft: width*0.01,paddingRight: width*0.02,fontFamily: "DinarTwoMedium_MRT",}}
+
+                </Header>
+                <Content
+                    style={{ paddingLeft: width * 0.01, paddingRight: width * 0.02, fontFamily: "DinarTwoMedium_MRT", }}
                 >
-                     
+
                     <Form
-                    style={{justifyContent:'center',textAlign:'center',}}
-                    > 
+                        style={{ justifyContent: 'center', textAlign: 'center', }}
+                    >
 
                         <Text
-                        style={styles.text}
-                        > 
-                                {lang.name}: {userProfile.PatientName}
-                        </Text>
-                        {/* <Text
-                        style={styles.text}
-                        > 
-                                {lang.Lname}: valikhanli 
-                        </Text>  */}
-                        <Text
-                        style={styles.text}                        
-                        > 
-                                {lang.national_code_}: {userProfile.PatientNationalCode}
-                        </Text>
-                       <List> 
-                           <ListItem itemDivider>
-                           </ListItem>
-                        </List>
-                        
-                        
-                        
-                        <Item> 
-                        <Collapse
-                        style={{width:width*0.95}}
+                            style={styles.text}
                         >
-                            <CollapseHeader>
+                            {lang.name}: {userProfile.PatientName}
+                        </Text>
+                        <Text
+                            style={styles.text}
+                        >
+                            {lang.national_code_}: {userProfile.PatientNationalCode}
+                        </Text>
+                        <List>
+                            <ListItem itemDivider>
+                            </ListItem>
+                        </List>
+
+
+
+                        <Item>
+                            <Collapse
+                                style={{ width: width * 0.95 }}
+                            >
+                                <CollapseHeader>
                                     <Text
-                                        style={styles.text}                        
-                                    > 
+                                        style={styles.text}
+                                    >
                                         {lang.cost_date}: {startDate}
                                     </Text>
-                            </CollapseHeader>
-                            <CollapseBody>
-                                <JalaliCalendarPicker
-                                onDateChange={this.onDateChange}
-                                textStyle={{fontFamily: "DinarTwoMedium_MRT",}}
-                                />
-                            </CollapseBody>
-                        </Collapse>
-                            
-                            
+                                </CollapseHeader>
+                                <CollapseBody>
+                                    <JalaliCalendarPicker
+                                        onDateChange={this.onDateChange}
+                                        textStyle={{ fontFamily: "DinarTwoMedium_MRT", }}
+                                    />
+                                </CollapseBody>
+                            </Collapse>
                         </Item>
-                        <Item picker>
+                        <Text
+                            style={styles.text}
+                        >
+                            {lang.cost_type}
+                        </Text>
+                        <Item picker >
                             <Picker
                                 mode="dropdown"
                                 iosIcon={<Icon name="ios-arrow-down-outline" />}
-                                style={[{ width: undefined, },styles.form_input]}
-                                placeholder={lang.cost_type}
-                                placeholderStyle={[{ color: "#bfc6ea" },styles.form_input]}
+                                style={[{ width: undefined, }, styles.form_input]}
+                                placeholderStyle={[{ color: "#bfc6ea" }, styles.form_input]}
                                 placeholderIconColor="#007aff"
-                                onValueChange={ (service) => ( this.setState({selectedPicker:service}) ) } 
+                                onValueChange={(service) => (this.setState({ selectedPicker: service }))}
                                 selectedValue={this.state.selectedPicker}
                             >
                                 {date_picker}
@@ -188,31 +190,29 @@ export default class cost_registration extends PureComponent {
                         <Item floatingLabel>
                             <Label style={styles.form_input} >{lang.cost_price}</Label>
                             <Input
-                            style={styles.form_input}
-                            keyboardType="numeric"
-                           // value={this.state.price}
-                           // onChange={(event)=>{this.create_currency_input()}}                     
-                                // placeholder={lang.cost_price}
-                             />
+                                style={styles.form_input}
+                                keyboardType="numeric"
+                                value={this.state.price}
+                                onChange={(event) => { this.setState({ price: event.nativeEvent.text }); }}
+                                onBlur={() => { this.create_currency_input(); }}
+                            />
                         </Item>
-                                    
-                        <Item 
-                         style= {{textAlign:'center',justifyContent:'center',marginTop: height*0.05,}}
+                        <Item
+                            style={{ textAlign: 'center', justifyContent: 'center', marginTop: height * 0.05, }}
                         >
                             <Label
-                            style={styles.form_input}
-                            >{lang.cost_price}</Label>    
+                                style={styles.form_input}
+                            >{lang.cost_price}</Label>
                         </Item>
-                        <Item> 
-                            <Button 
-                            style={{width:width*0.9,marginTop:height*0.02,marginBottom: height*0.025, marginLeft: width*0.025, marginRight: width*0.05 ,textAlign:'center',justifyContent:'center',fontFamily: "DinarTwoMedium_MRT",}}
-                            onPress={()=>{this.props.navigation.replace("upload_file",{userId:userid ,userProfile: userProfile});}}
+                        <Item>
+                            <Button
+                                style={{ width: width * 0.9, marginTop: height * 0.02, marginBottom: height * 0.025, marginLeft: width * 0.025, marginRight: width * 0.05, textAlign: 'center', justifyContent: 'center', fontFamily: "DinarTwoMedium_MRT", }}
+                                // onPress={() => { this.props.navigation.replace("upload_file", { userId: userid, userProfile: userProfile }); }}
+                                onPress={() => { this.test() }}
                             >
-
                                 <Text>{lang.next}</Text>
                             </Button>
                         </Item>
-                                    
                     </Form>
                 </Content>
             </Container>
